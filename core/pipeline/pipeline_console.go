65c9a6d269bc02f358df985746221b8573c86865
jindongyi <admin@vilanews.cn>
2016-07-04 10:17:29 +0800

    forked from https://github.com/hu17889/go_spider
    ## change list
    1.package import change (from github.com/hu17889/*** -> github.com/semua/***)
    2.core/page_items/items struct change (from map[string]string -> map[string]interface{})

--- core/pipeline/pipeline_console.go
+++ core/pipeline/pipeline_console.go
@@ -1,22 +1,35 @@
 package pipeline
 
 import (
-    "github.com/hu17889/go_spider/core/common/com_interfaces"
-    "github.com/hu17889/go_spider/core/common/page_items"
+	"github.com/semua/go_spider/core/common/com_interfaces"
+	"github.com/semua/go_spider/core/common/page_items"
 )
 
 type PipelineConsole struct {
 }
 
 func NewPipelineConsole() *PipelineConsole {
-    return &PipelineConsole{}
+	return &PipelineConsole{}
 }
 
 func (this *PipelineConsole) Process(items *page_items.PageItems, t com_interfaces.Task) {
-    println("----------------------------------------------------------------------------------------------")
-    println("Crawled url :\t" + items.GetRequest().GetUrl() + "\n")
-    println("Crawled result : ")
-    for key, value := range items.GetAll() {
-        println(key + "\t:\t" + value)
-    }
+	println("----------------------------------------------------------------------------------------------")
+	println("Crawled url :\t" + items.GetRequest().GetUrl() + "\n")
+	println("Crawled result : ")
+	for key, value := range items.GetAll() {
+		//println(key + "\t:\t" + value)
+		switch value.(type) {
+		case []string:
+			println(key, "\t:\t", value.([]string))
+			break
+		case map[string]string:
+			println(key, "\t:\t", value.(map[string]string))
+			break
+		case string:
+			println(key, "\t:\t", value.(string))
+			break
+		default:
+			panic("type match miss")
+		}
+	}
 }
--- core/pipeline/pipeline_file.go
+++ core/pipeline/pipeline_file.go
@@ -1,30 +1,44 @@
 package pipeline
 
 import (
-    "github.com/hu17889/go_spider/core/common/com_interfaces"
-    "github.com/hu17889/go_spider/core/common/page_items"
-    "os"
+	"fmt"
+	"os"
+
+	"github.com/semua/go_spider/core/common/com_interfaces"
+	"github.com/semua/go_spider/core/common/page_items"
 )
 
 type PipelineFile struct {
-    pFile *os.File
+	pFile *os.File
 
-    path string
+	path string
 }
 
 func NewPipelineFile(path string) *PipelineFile {
-    pFile, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0666)
-    if err != nil {
-        panic("File '" + path + "' in PipelineFile open failed.")
-    }
-    return &PipelineFile{path: path, pFile: pFile}
+	pFile, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		panic("File '" + path + "' in PipelineFile open failed.")
+	}
+	return &PipelineFile{path: path, pFile: pFile}
 }
 
 func (this *PipelineFile) Process(items *page_items.PageItems, t com_interfaces.Task) {
-    this.pFile.WriteString("----------------------------------------------------------------------------------------------\n")
-    this.pFile.WriteString("Crawled url :\t" + items.GetRequest().GetUrl() + "\n")
-    this.pFile.WriteString("Crawled result : \n")
-    for key, value := range items.GetAll() {
-        this.pFile.WriteString(key + "\t:\t" + value + "\n")
-    }
+	this.pFile.WriteString("----------------------------------------------------------------------------------------------\n")
+	this.pFile.WriteString("Crawled url :\t" + items.GetRequest().GetUrl() + "\n")
+	this.pFile.WriteString("Crawled result : \n")
+	for key, value := range items.GetAll() {
+		switch value.(type) {
+		case []string:
+			this.pFile.WriteString(key + "\t:\t" + fmt.Sprint(value) + "\n")
+			break
+		case map[string]string:
+			this.pFile.WriteString(key + "\t:\t" + fmt.Sprint(value) + "\n")
+			break
+		case string:
+			this.pFile.WriteString(key + "\t:\t" + value.(string) + "\n")
+			break
+		default:
+			panic("type match miss")
+		}
+	}
 }
